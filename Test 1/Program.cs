16ad7d862bce5ed3bd10959409bb96ba19682d2c
SaadAlsulami <SaadBAlsulami@gmail.com>
2024-11-25 19:00:19 +0300

    Add files via upload

---
+++ Test 1/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+class Program{
+
+    static void Main()
+    {
+       /* string FirstName = "Saad";
+        int age = 30;
+        double FavoriteNumber = 12;
+
+        Console.WriteLine(" Hello my name is "+ FirstName+ " I'M " + age+ " years old. and my favorite number is "+FavoriteNumber);*/
+
+
+       /* int x =5;
+        while (x <= 10)
+        {
+        Console.WriteLine(x);
+        x++;*
+      }*/
+
+
+
+      /* int num = 0;
+       Console.WriteLine(num <= 1);
+       num++; */
+
+
+       /*int Child = 1 ;
+       int teenager = 12;
+       int adult = 18;
+       Console.WriteLine("How old are you");
+       Console.ReadLine();*/
+
+       String[] A= {"January", " February", "March", "April"};
+       Console.WriteLine(A[3]);
+    
+    }
+}
